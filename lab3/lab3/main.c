5a78627b29fd77a04e14c626293421ecbc395d6c
strandafredde <75703920+strandafredde@users.noreply.github.com>
2023-02-10 11:10:26 +0100

    Add files via upload

---
+++ lab3/lab3/main.c
@@ -0,0 +1,18 @@
+/*
+ * lab3.c
+ *
+ * Created: 2023-02-10 11:09:19
+ * Author : stran
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
