3ea91c4e78e873b7f46e65c1c287be02010a1170
strandafredde <75703920+strandafredde@users.noreply.github.com>
2023-02-17 12:05:13 +0100

    Showing all three at once
    Computing primes but blink or button does not work after one "loop"

---
+++ lab3/Alt_2/main.c
@@ -0,0 +1,260 @@
+#include "tinythreads.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include <avr/io.h>
+#include <stdio.h>
+
+#define SCC_0  0x1551
+#define SCC_1  0x2080
+#define SCC_2  0x1E11
+#define SCC_3  0x1B11
+#define SCC_4  0x0B50
+#define SCC_5  0x1B41
+#define SCC_6  0x1F41
+#define SCC_7  0x0111
+#define SCC_8  0x1F51
+#define SCC_9  0x0B51
+
+//Array to store numbers
+int numArr[10] = {SCC_0, SCC_1 , SCC_2, SCC_3, SCC_4, SCC_5, SCC_6, SCC_7, SCC_8, SCC_9};
+
+int count;
+
+
+
+
+void LCD_init() {
+
+    /* Function: LCD_init
+     * -------------------
+     * initiation code for LCD
+     *
+     * returns: none
+     */
+
+    /* Asynchronous clock, 1/3 bias, 1/4 duty cycle,   25 segments */
+    LCDCRB = (1<<LCDCS) | (1<<LCDMUX1) | (1<<LCDMUX0) | (1<<LCDPM2) | (1<<LCDPM1) | (1<<LCDPM0);
+    /* Set volt to 3.35V */
+    LCDCCR = (1<<LCDCC3) |(1<<LCDCC2) | (1<<LCDCC1) | (1<<LCDCC0);
+    /* Clock division factor to 8, and prescaler to 16 */
+    LCDFRR = (1<<LCDCD2) | (1<<LCDCD1) | (1<<LCDCD0);
+    /* low power waveform, no frame interrupt, no blanking */
+    LCDCRA = (1<<LCDAB) | (1<<LCDEN);
+
+}
+
+void writeChar(char ch, int pos){
+
+    /* Function: WriteChar
+     * -------------------
+     * writes character (char ch) to LCD, at
+     * position (int pos) if within fixed range.
+     *
+     * returns: none
+     */
+
+    int i = 0;
+
+    /* checks if pos is within fixed range */
+    if(pos < 0 || pos > 5) {
+        return;
+    }
+
+    /* index select of character */
+    switch(ch){
+        case '0':
+            i = 0;
+            break;
+        case '1':
+            i = 1;
+            break;
+        case '2':
+            i = 2;
+            break;
+        case '3':
+            i = 3;
+            break;
+        case '4':
+            i = 4;
+            break;
+        case '5':
+            i = 5;
+            break;
+        case '6':
+            i = 6;
+            break;
+        case '7':
+            i = 7;
+            break;
+        case '8':
+            i = 8;
+            break;
+        case '9':
+            i = 9;
+            break;
+        default:
+            break;
+    }
+
+    /* writes translated char at pos to LCD */
+    switch(pos){
+        case 0:
+            LCDDR0  = (LCDDR0&0x00F0)  |  (numArr[i] & 0x000F);
+            LCDDR5  = (LCDDR5&0x00F0)  | ((numArr[i] & 0x00F0))>>4;
+            LCDDR10 = (LCDDR10&0x00F0) | ((numArr[i] & 0x0F00))>>8;
+            LCDDR15 = (LCDDR15&0x00F0) | ((numArr[i] & 0xF000))>>12;
+            break;
+        case 1:
+            LCDDR0  = (LCDDR0&0x000F)  |  ((numArr[i] & 0x000F))<<4;
+            LCDDR5  = (LCDDR5&0x000F)  |   (numArr[i] & 0x00F0);
+            LCDDR10 = (LCDDR10&0x000F) |  ((numArr[i] & 0x0F00))>>4;
+            LCDDR15 = (LCDDR15&0x000F) |  ((numArr[i] & 0xF000))>>8;
+            break;
+        case 2:
+            LCDDR1  = (LCDDR1&0x00F0)  |  (numArr[i] & 0x000F);
+            LCDDR6  = (LCDDR6&0x00F0)  |  ((numArr[i] & 0x00F0))>>4;
+            LCDDR11 = (LCDDR11&0x00F0) |  ((numArr[i] & 0x0F00))>>8;
+            LCDDR16 = (LCDDR16&0x00F0) |  ((numArr[i] & 0xF000))>>12;
+            break;
+        case 3:
+            LCDDR1  = (LCDDR1&0x000F)  |  ((numArr[i] & 0x000F))<<4;
+            LCDDR6  = (LCDDR6&0x000F)  |   (numArr[i] & 0x00F0);
+            LCDDR11 = (LCDDR11&0x000F) |  ((numArr[i] & 0x0F00))>>4;
+            LCDDR16 = (LCDDR16&0x000F) |  ((numArr[i] & 0xF000))>>8;
+            break;
+        case 4:
+            LCDDR2  = (LCDDR2&0x00F0)  | ((numArr[i] & 0x000F));
+            LCDDR7  = (LCDDR7&0x00F0)  | ((numArr[i] & 0x00F0))>>4;
+            LCDDR12 = (LCDDR12&0x00F0) | ((numArr[i] & 0x0F00))>>8;
+            LCDDR17 = (LCDDR17&0x00F0) | ((numArr[i] & 0xF000))>>12;
+            break;
+        case 5:
+            LCDDR2  = (LCDDR2&0x000F)  |  ((numArr[i] & 0x000F))<<4;
+            LCDDR7  = (LCDDR7&0x000F)  |  (numArr[i] & 0x00F0);
+            LCDDR12 = (LCDDR12&0x000F) |  ((numArr[i] & 0x0F00))>>4;
+            LCDDR17 = (LCDDR17&0x000F) |  ((numArr[i] & 0xF000))>>8;
+            break;
+        default:
+            break;
+    }
+
+}
+
+bool is_prime(long i) {
+
+    /* Function: is_prime
+     * -------------------
+     * determines whether (long i) is
+     * a prime value or not.
+     *
+     * returns: bool
+     */
+
+    for (int j = 2; j < i; j++) {
+        if(i % j == 0) {
+            /* is not prime */
+            return false;
+        }
+    }
+    /* is prime */
+    return true;
+}
+
+void printAt(long num, int pos) {
+	lock(&m_primes);
+    /* Function: printAt
+     * -------------------
+     * calls function to write char
+     * at fixed positions 0 and 1.
+     *
+     * returns: none
+     */
+
+    int pp = pos;
+    writeChar( (num % 100) / 10 + '0', pp);
+    pp++;
+    writeChar( num % 10 + '0', pp);
+	unlock(&m_primes);
+}
+
+void computePrimes(int pos) {
+
+    /* Function: computePrimes
+     * -------------------
+     * writes prime values to LCD
+     * at positions 0 and 1.
+     * returns: none
+     */
+    long n;
+    for(n = 1; ; n++) {
+        if (is_prime(n)) {
+            printAt(n, pos);
+        }
+    }
+}
+
+void button() {
+
+    /* Function: button
+     * -------------------
+     * code that counts button activations
+     * and writes them onto LCD.
+     *
+     * returns: none
+     */
+	lock(&m_button);
+	bool pressP4 = true;
+    PORTB = 0x0080;
+        if(!(PINB & (0X0080))) {
+
+            if(pressP4){
+                count ++;
+                pressP4 = false;
+            }
+        }
+        if(PINB & (0X0080)) {
+            pressP4 = true;
+        }
+        printAt(count, 4);
+}
+
+void blink() {
+
+    /* Function: blink
+     * -------------------
+     * activates segment of LCD at
+     * a 1Hz rate.
+     *
+     * returns: none
+     */
+	lock(&m_blink);
+		addBlinkCounter();
+		// blink every 0.5 second (one blink counter is 50ms, 10 = 0.5s)
+		if(getBlinkCounter() >= 10){
+			LCDDR18 = !LCDDR18;
+			resetBlinkCounter();
+		}
+
+}
+
+int main() {
+	//Initialize button
+	PORTB |= (1<<7);
+	
+	//Initialize clock 
+	CLKPR = (0x80);
+	CLKPR = (0x00);
+	
+	LCD_init();
+	lock(&m_blink);
+	lock(&m_button);
+	
+	
+	spawn(button, 0);
+	//yield();
+	spawn(blink, 0);
+	//yield();
+	computePrimes(0);
+	
+}
---
+++ lab3/Alt_2/tinythreads.c
@@ -0,0 +1,183 @@
+#include <setjmp.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "tinythreads.h"
+#include <stdio.h>
+
+#define NULL            0
+#define DISABLE()       cli()
+#define ENABLE()        sei()
+#define STACKSIZE       80
+#define NTHREADS        4
+#define SETSTACK(buf,a) *((unsigned int *)(buf)+8) = (unsigned int)(a) + STACKSIZE - 4; \
+*((unsigned int *)(buf)+9) = (unsigned int)(a) + STACKSIZE - 4
+
+struct thread_block {
+	void (*function)(int);   // code to run
+	int arg;                 // argument to the above
+	thread next;             // for use in linked lists
+	jmp_buf context;         // machine state
+	char stack[STACKSIZE];   // execution stack space
+};
+
+struct thread_block threads[NTHREADS];
+
+struct thread_block initp;
+
+thread freeQ   = threads;
+thread readyQ  = NULL;
+thread current = &initp;
+
+int initialized = 0;
+int blinkCounter = 0;
+
+mutex m_blink = MUTEX_INIT;
+mutex m_button = MUTEX_INIT;
+mutex m_primes = MUTEX_INIT;
+
+static void initialize(void) {
+	int i;
+	for (i=0; i<NTHREADS-1; i++)
+	threads[i].next = &threads[i+1];
+	threads[NTHREADS-1].next = NULL;
+	
+	
+
+	// Enable interrupts
+	EIMSK |= (1<<PCIE1); // 7th bit enabled for PCIE1 (1<<7)
+	PCMSK1 |= (1<<PCINT15); // 7th bit enabled for PCINT (1<<7)
+
+	// 	Set OC1A to 1
+	TCCR1A |= (1<<COM1A0) | (1<<COM1A1);
+	// 	Set the mode to CTC and prescaler to 1024
+	TCCR1B |= (1<<WGM12) | (1<<CS10) | (1<<CS12);
+	// 	Activate "timer output"
+	TIMSK1 |= (1<<OCIE1A);
+	
+	// Timer compare value
+	OCR1A = 391; // (8000000 / 1024) * 0.005
+	
+	// Set timer to 0
+	TCNT1 = 0;
+	initialized = 1;
+}
+
+void addBlinkCounter(){
+	// add 50ms to counter
+	DISABLE();
+	blinkCounter++;
+	ENABLE();
+}
+
+
+ISR(TIMER1_COMPA_vect) {
+	/*addBlinkCounter();*/
+	unlock(&m_blink);
+}
+
+ISR(PCINT1_vect) {
+	// Check the status of PORTB bit 7 before calling yield()
+	if (PORTB & (1<<7)) {
+		
+		unlock(&m_button);
+	}
+}
+
+
+int getBlinkCounter() {
+	DISABLE();
+	int temp = blinkCounter;
+	ENABLE();
+	return temp;
+}
+
+int resetBlinkCounter() {
+	DISABLE();
+	blinkCounter = 0;
+	ENABLE();
+}
+
+static void enqueue(thread p, thread *queue) {
+    if (*queue == NULL) {
+        *queue = p;
+    } else {
+        p->next = *queue;
+		*queue = p;
+
+    }
+}
+
+static thread dequeue(thread *queue) {
+	thread p = *queue;
+	if (*queue) {
+		*queue = (*queue)->next;
+		} else {
+		// Empty queue, kernel panic!!!
+		while (1) ;  // not much else to do...
+	}
+	return p;
+}
+
+static void dispatch(thread next) {
+	if (setjmp(current->context) == 0) {
+		current = next;
+		longjmp(next->context,1);
+	}
+}
+
+void spawn(void (* function)(int), int arg) {
+	thread newp;
+	DISABLE();
+	if (!initialized) initialize();
+	newp = dequeue(&freeQ);
+	newp->function = function;
+	newp->arg = arg;
+	newp->next = NULL;
+// 	SETSTACK(&newp->context, &newp->stack);
+// 	enqueue(newp, &readyQ);
+	if (setjmp(newp->context) == 1) {
+		ENABLE();
+		current->function(current->arg);
+		DISABLE();
+		enqueue(current, &freeQ);
+		dispatch(dequeue(&readyQ));
+	}
+	SETSTACK(&newp->context, &newp->stack);
+	//enqueue(newp, &readyQ);
+	enqueue(current, &readyQ);
+	dispatch(newp);
+	ENABLE();
+}
+
+void yield(void) {
+	
+	DISABLE();
+	enqueue(current,(&readyQ));
+	dispatch(dequeue(&readyQ));
+	ENABLE();
+}
+
+void lock(mutex *m) {
+	DISABLE();
+	if(m ->locked  == 0) {
+		m ->locked  = 1;
+	}
+	else {
+		enqueue(current,&(m ->waitQ));
+		dispatch(dequeue(&readyQ));
+	}
+	ENABLE();
+}
+
+void unlock(mutex *m) {
+	DISABLE();
+	if(m -> waitQ == NULL) {
+		m ->locked  = 0;
+	}
+	else {
+		enqueue(current, (&readyQ));
+		dispatch(dequeue(&(m -> waitQ)));
+	}
+	
+	ENABLE();
+}
